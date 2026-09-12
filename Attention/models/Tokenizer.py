6e71a55b0a9e703f30972c2045395ef5a05c38a7
iizom <iizom@example.com>
2026-09-12 16:04:54 +0800

    #

--- Attention/models/Tokenizer.py
+++ Attention/models/Tokenizer.py
@@ -1,5 +1,8 @@
 #Tokenizer.py
 
+from tokenizers import Tokenizer,models,trainers,pre_tokenizers
+import sentencepiece as spm
+
 class Tokenizer:
     def __init__(self, vocab):
         self.vocab = vocab
@@ -20,3 +23,26 @@ def build_vocab(captions, min_freq=1):
     vocab = [word for word, freq in counter.items() if freq >= min_freq]
     vocab = ['<pad>', '<sos>', '<eos>', '<unk>'] + vocab  # Add special tokens
     return vocab
+
+class ZZHTokenizer:
+    def __init__(self,vocab):
+        self.tok = Tokenizer(models.BPE(unk_token = "<unk>"))
+        self.tok.pre_tokenizer = pre_tokenizers.pre_tokenizers.ByteLevel(add_prefix_space=False)
+        self.trainer = trainers.BpeTrainer(vocab_size=len(vocab), special_tokens=["<pad>", "<sos>", "<eos>", "<unk>"])
+
+    def train_by_hf(self, vocab):
+        self.tok.train_from_iterator(vocab, trainer=self.trainer)
+        self.tok.save("zhtokenizer.json")
+
+def train_by_spm(corpus_path, vocab_size):
+    spm.SentencePieceTrainer.train(                                         
+    input=corpus_path, model_prefix="zh",                              
+    vocab_size=vocab_size, model_type="bpe",                                 
+    character_coverage=0.9995,          # 中文关键参数                  
+    pad_id=0, unk_id=1, bos_id=2, eos_id=3)     
+
+if __name__ == "__main__":
+    corpus_path = "/mnt/data/ML/dl/Attention/data/sanGuo/cap1.txt"
+    vocab_size = 5770
+    train_by_spm(corpus_path, vocab_size=vocab_size)
+    print("done")
